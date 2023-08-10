998f7fbe87a6b95c86023edebe03d15530d09bb4
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-08-10 22:05:22 +0900

    update userpage

--- src/app/users/me/page.tsx
+++ src/app/users/me/page.tsx
@@ -1,16 +1,29 @@
-import client from "@/service/client/client";
-import { getServerSession } from "next-auth";
+import UserProfile from '@/components/User/UserProfile'
+import client from '@/service/client/client'
+import { getServerSession } from 'next-auth'
 
 export default async function page() {
-	const data = await getServerSession();
-	if (!data?.user) return "no User";
+  const data = await getServerSession()
+  if (!data?.user) return 'no User'
 
-	const { name, email, image } = data.user;
-	const user = await client.user.findUnique({
-		where: {
-			email: email as string,
-		},
-	});
+  const { name, email, image } = data.user
+  console.log('data', data)
 
-	return user && <div>{user.id}</div>;
+  const user = await client.user.findUnique({
+    where: {
+      email: email as string,
+    },
+  })
+
+  if (!user) return 'user not found'
+
+  if (!user.name) user.name = name as string
+
+  return (
+    user && (
+      <div>
+        <UserProfile user={{ ...user, image }} />
+      </div>
+    )
+  )
 }
